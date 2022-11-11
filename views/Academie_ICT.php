62cc58912ce9162e1a3568d2e684c47c11f476cc
Pan1n1 <330507@student.mboutrecht.nl>
2022-11-11 10:02:01 +0100

    Navbar fix en start index page

--- views/Academie_ICT.php
+++ views/Academie_ICT.php
@@ -1,6 +1,10 @@
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
 <link rel="stylesheet" href="../../assets/css/maps.css">
+
+<body>
+<?php include("./navbar.php")  ?>
 
 <main>
     <h1>ICT</h1>
@@ -12,3 +16,4 @@
     </div>
     <p>Lorem pisum pikies in je visum</p>
 </main>
+</body>
--- views/index.php
+++ views/index.php
@@ -6,38 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../css/style.css"> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/maps.css"> 
   </head>
 
 <body>
+<?php include("./navbar.php") ?>
 
-<nav class="navbar navbar-expand-lg bg-primary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Open dag</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="login.php">log in</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Courses
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
 
 <footer class="bg-primary text-white pt-5 pb-4"> 
   <div class="container text-center text-md-left"> 
@@ -50,15 +24,21 @@
 
     </div>
   </div>
-
-
-
 </footer>
+
+<main>
+
+<a href="./login.php">Login</a>
+<a href="./maps.php">Maps</a>
+
+</main>
+
+
+
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </body>
 </html>
 
-<a href="./login.php">Login</a>
-<a href="./maps.php">Maps</a>
 
