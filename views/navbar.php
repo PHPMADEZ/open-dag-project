1ee70100c5d8551e2284dbd595b66b977be47b65
Pan1n1 <330507@student.mboutrecht.nl>
2022-11-15 11:19:08 +0100

    Navbar homebuttons werken

--- views/navbar.php
+++ views/navbar.php
@@ -1,14 +1,14 @@
 
 <nav class="navbar navbar-expand-lg bg-primary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Open dag</a>
+    <a class="navbar-brand" href="./index.php">Open dag</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="./index.php">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="login.php">log in</a>
